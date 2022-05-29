dbd6b0156783542d0ed2558f01e7dd085b36145d
Kraynn <75184473+Kraynn@users.noreply.github.com>
2022-05-29 02:18:45 +0200

    Update img_extract.py

--- img_extract.py
+++ img_extract.py
@@ -38,7 +38,7 @@ for cat_url in soup.select("li a[href*=category]"):
     categories.append(base_index+cat_url.get("href"))                                         
 categories.pop(0)
 
-for url in categories[:2]:
+for url in categories:
     browse_category(url)
     npages = []
     page = requests.get(url)
