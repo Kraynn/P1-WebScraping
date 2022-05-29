bc7cea15ec67045739a7496cf4ec0e3302a003f6
Kraynn <75184473+Kraynn@users.noreply.github.com>
2022-05-29 02:35:06 +0200

    Update ETL_BookToScrape.py
    Modification du script afin d'itérer sur l'ensemble des catégories (y compris celles comprenant plusieurs pages) et modification de la méthode de parsing d'URL avec la librairie regex.

--- ETL_BookToScrape.py
+++ ETL_BookToScrape.py
@@ -3,80 +3,75 @@ import requests
 import csv
 import re
 
-with open("Travel.csv", "w", newline="") as f:
-        header = ['title', 'category', 'product_page_url', 'upc','price_including_tax', 'price_excluding_tax','product_description', 'number_available', 'review_rating', 'image_url']
+def load_data(data,filename):
+    with open(f"{filename}.csv", "a", newline="") as f:
         write = csv.writer(f)
+        header = ['title', 'category', 'product_page_url', 'upc','price_including_tax',
+        'price_excluding_tax','product_description', 'number_available', 'review_rating', 'image_url']
         write.writerow(header)
+        write.writerows(data)
 
-
-def extract_data(data):
-    rows = data
-    with open("Travel.csv", "a", newline="") as f:
-        write = csv.writer(f)
-        write.writerow(rows)
-
-def parse(url):
+def parse_product(url):
     page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')                                                           
-                                                    
-    title = soup.find('h1').text                                              
+    soup = BeautifulSoup(page.content, 'html.parser') 
 
+    print(url)                                                          
+                                                    
+    title = soup.find('h1').text                                            #Title  
     img = soup.find("img").get("src")
-    img = "https://books.toscrape.com" + img[5:]                            
-                                     
-    summary = soup.select("article[class=product_page]>p")                  
-    
-    rating = soup.find(class_=re.compile("star-rating.*"))                  
+    img = "https://books.toscrape.com" + img[5:]                            #Image Source (lien à modifier)                                 
+    summary = soup.select("article[class=product_page]>p")                  #Description Product
+    rating = soup.find(class_=re.compile("star-rating.*"))                  #Review rating
     rating = rating.get_attribute_list("class")[1]
-    
     price = soup.find('p', class_="price_color").text
     price_tax = price                                                        
-
-    upc = soup.find("td").text                                             
-
-    cat = soup.find("ul", class_="breadcrumb")                               
+    upc = soup.find("td").text                                              
+    cat = soup.find("ul", class_="breadcrumb")                              
     cat = cat.find_next("a")
     cat = cat.find_next("a")
     category = cat.find_next("a").text
-    
-    stock = soup.find("p", class_="instock availability").get_text()        
+    stock = soup.find("p", class_="instock availability").get_text().strip()        
 
     return (title, category, url, upc, price, price_tax, summary, stock, rating, img)
     
-def browse_product_url(categories_url):
-
-    page = requests.get(categories_url)
+def browse_category(category):
+    page = requests.get(category["url"])
     soup = BeautifulSoup(page.content, 'html.parser')
 
     product_data = []
-
     for url in soup.select("div[class=image_container]>a"):
         base_url = "https://books.toscrape.com/catalogue/"
         _url = url.get("href")
-        
-        if _url[:9] == '../../../':
-            _url = base_url + _url[9:]
-        else:
-            _url = base_url + _url[6:]
-       
-        parsed_url = parse(_url)                              
+        _url = re.sub("^[../]+", base_url, _url)
+        parsed_url = parse_product(_url)                              
         product_data.append(parsed_url)
-
-    for data in product_data:
-        extract_data(data)
-
+    load_data(product_data,category["name"])
 
 index = "http://books.toscrape.com/index.html"
 page = requests.get(index)
 soup = BeautifulSoup(page.content, 'html.parser')
 
-categories_url = []
+categories = []
 for cat_url in soup.select("li a[href*=category]"):
     base_index = "https://books.toscrape.com/"
-    categories_url.append(base_index+cat_url.get("href"))                                         
-categories_url.pop(0)
+    name = cat_url.text.strip()  
+    category = {"name":name, "url":base_index+cat_url.get("href")}
+    categories.append(category)                                          
+categories.pop(0)
 
-for url in categories_url[:1]:
-    browse_product_url(url)
-
+for category in categories:
+    browse_category(category)
+    
+    npages = []
+    page = requests.get(category["url"])
+    soup = BeautifulSoup(page.content, 'html.parser')
+    if soup.find(class_=re.compile("next.*")):
+        pass
+    for page_url in soup.select("li[class=next] a"):
+        npage = re.sub("index.html", page_url.get("href"), category["url"])
+        name = soup.find("h1").text
+        category = {"name": name, "url": npage}
+        npages.append(category)
+        for npage in npages:
+            browse_category(npage)
 
